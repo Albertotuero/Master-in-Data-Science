08b8680ff59475b738fe494d0095081a75faf128
Alberto Tuero <dsc@localhost.localdomain>
2015-11-14 13:30:15 +0100

    Parametrized shell script

--- model_more_engines.sh
+++ model_more_engines.sh
@@ -5,8 +5,8 @@
 
 #csvcut -d '^' -c nb_engines,model optd_aircraft.csv | sort | unique 
 
-# Read the model with more engines
-csvsort -d '^' -c nb_engines optd_aircraft.csv | csvcut -c model | tail -1
+# Read the model with more engines, the input file is a parameter
+csvsort -d '^' -c nb_engines $1 | csvcut -c model | tail -1
 
 # Teacher solution
 # csvsort -d '^' -r -c nb_engines optd_aircraft.csv | head -2 | csvcut -c model | tail -1
