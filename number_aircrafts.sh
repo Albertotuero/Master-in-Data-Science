d00d12657944f82875d5ed722224cdef341db3a8
Alberto Tuero <dsc@localhost.localdomain>
2015-11-21 12:51:41 +0100

    Eliminacion de scripts

--- number_aircrafts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Calculates the number of aircrafts in optd_aircrafts.csv file with a specified number of engines
-echo The number of aircrafts with $1 engines is 
-csvcut -d '^' -c nb_engines,iata_code $2 | grep -e "^$1" | wc -l 
